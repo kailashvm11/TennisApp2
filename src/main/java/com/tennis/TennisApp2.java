7b1d449e80bbff41dedeb74dd0541c4ec46f4453
Rajesh <kailashvm@yahoo.com>
2021-05-16 07:01:28 +0200

    Initial Commit

---
+++ src/main/java/com/tennis/TennisApp2.java
@@ -0,0 +1,6 @@
+package com.tennis;
+
+public class TennisApp2 {
+    public static void main( String[] args ) {
+    }
+}
---
+++ src/test/java/com/tennis/TennisApp2Test.java
@@ -0,0 +1,11 @@
+package com.tennis;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TennisApp2Test {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
